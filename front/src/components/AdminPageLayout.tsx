5cabaabe95fa495b6bc6dc0f9c6935c749ff8ec6
peterramirez18 <peterramirez1812@gmail.com>
2024-01-15 15:27:26 -0400

    feat: studios page tablet added

--- front/src/components/AdminPageLayout.tsx
+++ front/src/components/AdminPageLayout.tsx
@@ -1,6 +1,6 @@
 const AdminPageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <section className=" min-h-[800px] bg-gray100 flex-1 m-5 ml-0 rounded-2xl p-5">
+    <section className="bg-gray100 flex-1 m-5 ml-0 rounded-2xl p-5">
       {children}
     </section>
   );
